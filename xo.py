207cc6d03fe14080adbe45f4cf77918ba969e4d7
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-20 09:03:44 -0400

    Update xo.py

--- xo.py
+++ xo.py
@@ -1,1 +1,26 @@
-{\rtf1}
+class Solution:
+    def count_XO(self, string):
+            #type string: string
+            #return type: boolean
+            x = 0
+            o = 0
+            #TODO: Write code below to returnn a boolean value with the solution to the prompt.
+            for s in string:
+                if string == "x":
+                    x = x+1
+                if string == "o":
+                    o = 0+1
+            if x == o:
+                 return True
+            else:
+                 return False
+            pass
+                
+def main():
+    input1=input()
+    tc1 = Solution()
+    ans = tc1.count_XO(input1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
